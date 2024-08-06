fd504b978b705992aef7abb57331930235ace549
ianand3977 <anandgu2002@gmail.com>
2024-08-06 15:58:14 +0530

    icon update

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,7 +15,7 @@ const About = () => {
           <p>
             Hi, my name is <b>Anand Ubarhande</b> and I am from Aurangabad, India.
             I'm a <b>MERN stack web developer</b> and a final year college student pursuing <b>BTech in CSE</b>. <br/><br/>
-            I have done summer internship in  <b>Data Structures and Algorithms</b> at GeekForGeeks.
+            I have done Summer Training in <b>Data Structures and Algorithms</b> at GeekForGeeks.
             I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
             I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
             Apart from coding I love to play Badminton
